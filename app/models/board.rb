d193bba69a6b6385c3aa07a7e37b06119a42a983
Naftali Kulik <nkulik1994@gmail.com>
2022-07-03 11:29:06 -0400

    added Board class method to generate new boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,9 @@
 class Board < ActiveRecord::Base
     has_many :played_boards
     has_many :players, through: :played_boards
+
+    def self.generate_board
+        layout = (0..99).to_a.shuffle.take(25)
+        find_or_create_by(layout: layout.join(' '))
+    end
 end
