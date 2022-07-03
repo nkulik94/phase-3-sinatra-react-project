a2b23abeaa4212a9de41e66068a2365ddc5b686b
Naftali Kulik <nkulik1994@gmail.com>
2022-07-03 09:39:33 -0400

    added columns to boards table migration

--- db/migrate/20220703131336_create_boards_table.rb
+++ db/migrate/20220703131336_create_boards_table.rb
@@ -1,4 +1,10 @@
 class CreateBoardsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :boards do |t|
+      t.string :layout
+      t.integer :line_high_score
+      t.integer :x_high_score
+      t.integer :full_high_score
+    end
   end
 end
