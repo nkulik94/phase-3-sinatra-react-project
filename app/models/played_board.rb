5efe81d171d6388405328512507f8f2db94f5815
Naftali Kulik <nkulik1994@gmail.com>
2022-07-03 13:16:26 -0400

    wrote some methods for PlayedBoard instances to facilitate playing the game

--- app/models/played_board.rb
+++ app/models/played_board.rb
@@ -1,4 +1,24 @@
 class PlayedBoard < ActiveRecord::Base
     belongs_to :board
     belongs_to :player
+
+    def unused_nums_arr
+        self.unused_nums.split(' ')
+    end
+
+    def update_unused_nums num
+        nums_arr.delete num
+        update(unused_nums: nums_arr.join(' '))
+    end
+
+    def pick_num nums_arr
+        new_num = nums_arr.sample
+        update_unused_nums(new_num)
+        new_num
+    end
+
+    def check_for_match num
+        layout = board.layout.split(' ')
+        matched_num = layout.find { |board_num| board_num == num}
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 puts "🌱 Seeding spices..."
 
+# generate 20 unique boards
 Board.generate_board until Board.all.count == 20
+
+#generate fake users
+5.times do
+    name = Faker::Name.name
+    username = Faker::Beer.brand.gsub(/\s+/, "")
+    password = Faker::Types.rb_string
+    Player.create(name: name, username: username, password: password)
+end
 
 puts "✅ Done seeding!"
