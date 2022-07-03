94f5c45fb96d34ffe601d022ee9b0b9dfef26b36
Naftali Kulik <nkulik1994@gmail.com>
2022-07-03 09:44:33 -0400

    added columns to played_boards table migration

--- db/migrate/20220703132205_create_played_boards_table.rb
+++ db/migrate/20220703132205_create_played_boards_table.rb
@@ -1,4 +1,14 @@
 class CreatePlayedBoardsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :played_boards do |t|
+      t.integer :board_id
+      t.integer :player_id
+      t.string :unused_nums
+      t.integer :turn_count
+      t.string :filled_spaces
+      t.integer :turns_to_line
+      t.integer :turns_to_x
+      t.integer :turns_to_full
+    end
   end
 end
