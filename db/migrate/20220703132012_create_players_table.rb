cd76b25fb2ac180e27a6008677b920f942ee2228
Naftali Kulik <nkulik1994@gmail.com>
2022-07-03 09:34:23 -0400

    added columns to players table migration

--- db/migrate/20220703132012_create_players_table.rb
+++ db/migrate/20220703132012_create_players_table.rb
@@ -1,4 +1,9 @@
 class CreatePlayersTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :players do |t|
+      t.string :name
+      t.string :username
+      t.string :password
+    end
   end
 end
