e4e068d0dc79cb0aa31f4b410ff194b2b16762b9
Naftali Kulik <nkulik1994@gmail.com>
2022-07-03 13:38:22 -0400

    built out more methods to handle gameplay

--- app/models/played_board.rb
+++ app/models/played_board.rb
@@ -3,22 +3,42 @@ class PlayedBoard < ActiveRecord::Base
     belongs_to :player
 
     def unused_nums_arr
-        self.unused_nums.split(' ')
+        unused_nums.split(' ')
+    end
+
+    def layout_arr
+        board.layout.split(' ')
+    end
+
+    def filled_spaces_arr
+        filled_spaces.split(' ')
     end
 
     def update_unused_nums num
-        nums_arr.delete num
+        unused_nums_arr.delete num
         update(unused_nums: nums_arr.join(' '))
+    end
+
+    def update_filled_spaces index
+        filled_spaces_arr.push(index)
+    end
+
+    def has_line?
+        Board::LINE_WIN_COMBINATIONS.find { |combo| (combo - filled_spaces).empty? }
     end
 
     def pick_num nums_arr
         new_num = nums_arr.sample
         update_unused_nums(new_num)
-        new_num
+        check_for_match(new_num)
     end
 
     def check_for_match num
-        layout = board.layout.split(' ')
-        matched_num = layout.find { |board_num| board_num == num}
+        matched_num_index = layout_arr.find_index num
+        handle_match(matched_num_index) if matched_num_index
+    end
+
+    def handle_match index
+
     end
 end
