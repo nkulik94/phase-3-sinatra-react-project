bc2b4f9aa6c79a67f2626c4f82532cf18a76cb18
Naftali Kulik <nkulik1994@gmail.com>
2022-07-04 14:16:38 -0400

    started fixing methods to simulate gameplay

--- app/models/played_board.rb
+++ app/models/played_board.rb
@@ -2,58 +2,79 @@ class PlayedBoard < ActiveRecord::Base
     belongs_to :board
     belongs_to :player
 
-    def unused_nums_arr
-        unused_nums.split(' ')
-    end
-
-    def layout_arr
-        board.layout.split(' ')
-    end
-
-    def filled_spaces_arr
-        filled_spaces ? filled_spaces.split(' ') : []
-    end
-
     def count_turn
-        count = turn_count ? turn_count : 0
+        count = self.turn_count ? self.turn_count : 0
         update(turn_count: count + 1)
     end
 
     def remove_from_unused num
+        unused_nums_arr = self.unused_nums.split(' ')
         unused_nums_arr.delete num
         update(unused_nums: unused_nums_arr.join(' '))
     end
 
     def update_filled_spaces index
+        filled_spaces_arr = self.filled_spaces ? self.filled_spaces.split(' ') : []
         filled_spaces_arr.push(index)
         update(filled_spaces: filled_spaces_arr.join(' '))
     end
 
     def has_line?
-        turns_to_line || Board::LINE_WIN_COMBINATIONS.find { |combo| (combo - filled_spaces_arr).empty? }
+        filled_spaces_arr = self.filled_spaces ? self.filled_spaces.split(' ') : []
+        self.turns_to_line || Board::LINE_WIN_COMBINATIONS.find { |combo| (combo - filled_spaces_arr).empty? }
     end
 
     def has_x?
-        turns_to_x || (Board::X_WIN - filled_spaces_arr).empty?
+        filled_spaces_arr = self.filled_spaces ? self.filled_spaces.split(' ') : []
+        self.turns_to_x || (Board::X_WIN - filled_spaces_arr).empty?
     end
 
     def is_full?
+        filled_spaces_arr = self.filled_spaces ? self.filled_spaces.split(' ') : []
         filled_spaces_arr.count == 25
     end
 
     def get_match num
+        layout_arr = self.board.layout.split(' ')
         layout_arr.find_index num
+    end
+
+    def update_high_score high_score
+        if !board[high_score] || turn_count < board[high_score]
+            board.update(high_score: turn_count)
+        end
+    end
+
+    def update_full_score
+        update(turns_to_full: turn_count)
+        #update_high_score(:full_high_score)
+    end
+
+    def update_x_score
+        update(turns_to_x: turn_count)
+        #update_high_score(:x_high_score)
+    end
+
+    def update_line_score
+        update(turns_to_line: turn_count)
+        #update_high_score(:line_high_score)
     end
 
     def handle_match index
         update_filled_spaces(index)
-        if is_full?
-            update(turns_to_full: turn_count)
-        elsif !turns_to_x && has_x?
-            update(turns_to_x: turn_count)
-        elsif !turns_to_line && has_line?
-            update(turns_to_line: turn_count)
-        end
+        update_full_score if is_full?
+        update_x_score if !self.turns_to_x && has_x?
+        update_line_score if !self.turns_to_line && has_line?
+        # if is_full?
+        #     update(turns_to_full: turn_count)
+        #     update_high_score(:full_high_score)
+        # elsif !self.turns_to_x && has_x?
+        #     update(turns_to_x: turn_count)
+        #     update_high_score(:x_high_score)
+        # elsif !self.turns_to_line && has_line?
+        #     update(turns_to_line: turn_count)
+        #     update_high_score(:line_high_score)
+        # end
     end
 
     def play_round num
@@ -64,7 +85,6 @@ class PlayedBoard < ActiveRecord::Base
     end
 
     def sim_play
-        play_round(unused_nums_arr.sample)
-        sim_play unless is_full?
+        play_round(unused_nums.split(' ').sample)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,15 @@ Board.generate_board until Board.all.count == 20
     Player.create(name: name, username: username, password: password)
 end
 
+#simulate each user playing each board
+# Player.all.each do |player|
+#     Board.all.each do |board|
+#         unused_nums = (0..99).to_a
+#         unused_nums = unused_nums.join(' ')
+#         new_game = PlayedBoard.create(player_id: player[:id], board_id: board[:id], unused_nums: unused_nums)
+#         #new_game.sim_play until new_game.is_full?
+#         puts new_game
+#     end
+# end
+
 puts "✅ Done seeding!"
