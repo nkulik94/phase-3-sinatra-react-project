2466912729d2f372dafa7679cb54dd327edb00b4
Naftali Kulik <nkulik1994@gmail.com>
2022-07-04 11:03:07 -0400

    added and modified more methods for gameplay

--- app/models/played_board.rb
+++ app/models/played_board.rb
@@ -11,31 +11,40 @@ class PlayedBoard < ActiveRecord::Base
     end
 
     def filled_spaces_arr
-        filled_spaces.split(' ')
+        filled_spaces ? filled_spaces.split(' ') : []
+    end
+
+    def count_turn
+        count = turn_count ? turn_count : 0
+        update(turn_count: count + 1)
     end
 
     def update_unused_nums num
         unused_nums_arr.delete num
-        update(unused_nums: nums_arr.join(' '))
+        update(unused_nums: unused_nums_arr.join(' '))
     end
 
     def update_filled_spaces index
         filled_spaces_arr.push(index)
+        update(filled_spaces: filled_spaces_arr.join(' '))
     end
 
     def has_line?
-        Board::LINE_WIN_COMBINATIONS.find { |combo| (combo - filled_spaces).empty? }
+        Board::LINE_WIN_COMBINATIONS.find { |combo| (combo - filled_spaces_arr).empty? }
     end
 
-    def pick_num nums_arr
-        new_num = nums_arr.sample
+    def has_x?
+        (Board::X_WIN - filled_spaces_arr).empty?
+    end
+
+    def pick_num
+        new_num = unused_nums_arr.sample
         update_unused_nums(new_num)
         check_for_match(new_num)
     end
 
     def check_for_match num
-        matched_num_index = layout_arr.find_index num
-        handle_match(matched_num_index) if matched_num_index
+        layout_arr.find_index num
     end
 
     def handle_match index
